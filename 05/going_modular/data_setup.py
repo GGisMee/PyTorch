70d811655be0827764d51ac6f9f6e2db6248d20e
GGisMee <gustav.gamstedt@outlook.com>
2023-08-22 10:59:49 +0200

    05 created

---
+++ 05/going_modular/data_setup.py
@@ -0,0 +1,7 @@
+import os
+import requests
+import zipfile
+from pathlib import Path
+
+data_path = Path('data/')
+image_path = data_path / 'pizza_steak_sushi'
---
+++ 05/going_modular/train.py
@@ -0,0 +1,4 @@
+import os
+import torch as pt
+import data_setup, engine, model_builder, utils
+from torchvision import transforms
